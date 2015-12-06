65849dde394a40c941862fb0bfa4720661a354f2
William Armando Forero Bermeo <williamforerob@gmail.com>
2015-12-06 05:41:29 -0500

    Primer Commmit de Galeria
    En este primer envío se define la estructura de la vista

---
+++ app/src/main/java/com/example/william/newgalery/controlador/Galeria.java
@@ -0,0 +1,7 @@
+package com.example.william.newgalery.controlador;
+
+/**
+ * Created by william on 06/12/2015.
+ */
+public class Galeria {
+}
---
+++ app/src/main/java/com/example/william/newgalery/modelo/Recursos.java
@@ -0,0 +1,7 @@
+package com.example.william.newgalery.modelo;
+
+/**
+ * Created by william on 06/12/2015.
+ */
+public class Recursos {
+}
