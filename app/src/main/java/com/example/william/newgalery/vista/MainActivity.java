4b424e01902edcd8f6ca211c864a5356b5ed613e
William Armando Forero Bermeo <williamforerob@gmail.com>
2015-12-06 06:20:49 -0500

    Implementación de escuchador
    Se implementa el escuchador en cada una de las imagenes

--- app/src/main/java/com/example/william/newgalery/vista/MainActivity.java
+++ app/src/main/java/com/example/william/newgalery/vista/MainActivity.java
@@ -4,36 +4,58 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.*;
+import android.widget.ImageView; // Se importa esta libreria para que nos permita imoplementar los escuchadores
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.william.newgalery.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener { //Se implemente el escuchador click
+
+    private ImageView imgUno, imgDos, imgTres, imgPrincipal;
+    private String nombreImagen;
+    private TextView tvInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Mapeamos las imagenes de la galería
+        imgUno = (ImageView) findViewById(R.id.img_uno);
+        imgDos = (ImageView) findViewById(R.id.img_dos);
+        imgTres = (ImageView) findViewById(R.id.img_tres);
+        tvInfo = (TextView) findViewById(R.id.tvInfo);
+
+        //Le asignamos el escuchador a cada una de las imágenes
+        imgUno.setOnClickListener(this);
+        imgDos.setOnClickListener(this);
+        imgTres.setOnClickListener(this);
+
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        nombreImagen = null;
+        try{
+            switch (v.getId()) {
+                case R.id.img_uno:
+                    nombreImagen = v.getResources().getResourceEntryName(R.id.img_uno);
+                    break;
+                case R.id.img_dos:
+                    nombreImagen = v.getResources().getResourceEntryName(R.id.img_dos);
+                    break;
+                case R.id.img_tres:
+                    nombreImagen = v.getResources().getResourceEntryName(R.id.img_tres);
+                    break;
+            }
+            Toast.makeText(this, "imagen: " + nombreImagen, Toast.LENGTH_SHORT).show();
+        }catch(Exception e){
+            tvInfo.setText(e.toString());
         }
 
-        return super.onOptionsItemSelected(item);
     }
 }
